ece80a409fb162c5769e77a900257015c5b56ac0
Neamul Kabir Emon <neamulkabiremon@gmail.com>
2025-06-03 23:28:51 +0600

    Initial commit: setup MERN Expense Tracker project with backend, frontend, and docs

--- backend/server.js
+++ backend/server.js
@@ -32,4 +32,4 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://0.0.0.0:${PORT}`));
